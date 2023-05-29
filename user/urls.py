4421b4319017ae2878d62abb906737141f7c1fbd
iis2mx <133914233+iis2mx@users.noreply.github.com>
2023-05-29 14:58:58 +0300

    first

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('signout', views.signout, name='signout'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+
+
+
+
+]
